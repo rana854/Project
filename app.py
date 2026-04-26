51ee25a98431a89edd59032219573d79ab1f9a3f
RanaTarek566 <ranatarekmahmoud566@gmail.com>
2026-04-26 15:25:02 +0200

    2 commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+
+app = Flask(__name__)
+
+REQUEST_COUNT = Counter('requests_total', 'Total Requests')
+
+@app.route("/")
+def home():
+    REQUEST_COUNT.inc()
+    return "Hello DevOps Project"
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "ok"})
+
+@app.route("/metrics")
+def metrics():
+    return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
